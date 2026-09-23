bc2e9b214d5e9b89433a07ec70fdce04e8e647c5
VishalMohindra <moh.vishal@gmail.com>
2026-09-23 09:00:10 +0530

    Update Vite base for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/feedback-app-final/",   // MUST match GitHub repo name!!
+  base:"/",   
 })
