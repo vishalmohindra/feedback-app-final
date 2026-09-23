54ef39c5161752f9f2945cdff702611706e6f12a
VishalMohindra <moh.vishal@gmail.com>
2026-09-23 09:29:31 +0530

    Fix Button import case

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from 'react';
 import Card from './shared/Card';
-import Button from './shared/button';
+import Button from './shared/Button';
 import RatingSelect from './RatingSelect';
 import FeedbackContext from '../Contexts/FeedbackContext';
 
